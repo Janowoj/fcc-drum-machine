6776f1862f8ab9f01b8a7df9bf81773f4da319a3
Jan Wojcik <jan.jozef.wojcik@gmail.com>
2023-08-09 11:33:52 +0200

    "some css improvements"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeKey, setActiveKey] = useState('');
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      playSound(event.key.toUpperCase());
+    });
+  }, []);
+
+  const drumPads = [
+    {
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+      name: "Heater-1"
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+      name: "Heater-2"
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+      name: "Heater-3"
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+      name: "Heater-4"
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+      name: "Clap"
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+      name: "Open-HH"
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+      name: "Kick-n'-Hat"
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+      name: "Kick"
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+      name: "Closed-HH"
+    }
+  ];
+
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    console.log(audio);
+    audio.play();
+    setActiveKey(selector);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Drum Machine</h1>
+      <div id='drum-machine'>
+        <div className='drum-pads'>
+          {drumPads.map((drumPad) => (
+            <div
+              key={drumPad.src}
+              onClick={() => {
+                playSound(drumPad.text);
+              }}
+              className='drum-pad'
+              id={drumPad.src}>
+              {drumPad.text}
+              <audio
+                className='clip'
+                id={drumPad.text}
+                src={drumPad.src}>
+              </audio>
+            </div>
+          ))}
+        </div>
+        <div className='controls-container'>
+          <div id='display'>{activeKey}</div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
